13711d08ebaf513b1cc992b014b83a9f76a831f3
Thomas Markovich <thomasmarkovich+git@gmail.com>
2017-07-27 09:49:32 -0400

    Added cepy wrapper

---
+++ cepy/__init__.py
@@ -0,0 +1,130 @@
+# Copyright 2017 Gamalon, Inc.
+#
+# This file is part of cepy.
+#
+# cepy is distributed under the Apache 2.0 license.
+
+from .cepy import airy
+from .cepy import bdtrc
+from .cepy import bdtr
+from .cepy import bdtri
+from .cepy import beta
+from .cepy import betaln
+from .cepy import lbeta
+from .cepy import btdtr
+from .cepy import cbrt
+from .cepy import chbevl
+from .cepy import chdtrc
+from .cepy import chdtr
+from .cepy import chdtri
+from .cepy import cpmul
+from .cepy import dawsn
+from .cepy import ei
+from .cepy import ellie
+from .cepy import ellik
+from .cepy import ellpe
+from .cepy import ellpj
+from .cepy import ellpk
+from .cepy import euclid
+from .cepy import rdiv
+from .cepy import rmul
+from .cepy import rsub
+from .cepy import radd
+from .cepy import fdtrc
+from .cepy import fdtr
+from .cepy import fdtri
+from .cepy import fresnl
+from .cepy import gam
+from .cepy import gammainc
+from .cepy import gamln
+from .cepy import lgam
+from .cepy import gdtr
+from .cepy import gdtrc
+from .cepy import gels
+from .cepy import hyp2f1
+from .cepy import hyperg
+from .cepy import hyp2f0
+from .cepy import i0
+from .cepy import i0e
+from .cepy import i1
+from .cepy import i1e
+from .cepy import igamc
+from .cepy import igam
+from .cepy import igami
+from .cepy import incbet
+from .cepy import incbi
+from .cepy import iv
+from .cepy import j0
+from .cepy import y0
+from .cepy import y1
+from .cepy import j1
+from .cepy import jn
+from .cepy import jv
+from .cepy import k0
+from .cepy import k0e
+from .cepy import k1
+from .cepy import k1e
+from .cepy import kn
+from .cepy import smirnov
+from .cepy import kolmogorov
+from .cepy import smirnovi
+from .cepy import kolmogi
+from .cepy import levnsn
+from .cepy import minv
+from .cepy import nbdtrc
+from .cepy import nbdtr
+from .cepy import nbdtri
+from .cepy import ndtr
+from .cepy import erfc
+from .cepy import ndtri
+from .cepy import pdtrc
+from .cepy import pdtr
+from .cepy import pdtri
+from .cepy import plancki
+from .cepy import planckc
+from .cepy import planckd
+from .cepy import planckw
+from .cepy import p1evl
+from .cepy import polatn
+from .cepy import polsqt
+from .cepy import polsin
+from .cepy import polcos
+from .cepy import polrt
+from .cepy import polylog
+from .cepy import polini
+from .cepy import polprt
+from .cepy import polclr
+from .cepy import polmov
+from .cepy import polmul
+from .cepy import poladd
+from .cepy import polsub
+from .cepy import poldiv
+from .cepy import polsbt
+from .cepy import poleva
+from .cepy import psi
+from .cepy import revers
+from .cepy import rgamma
+from .cepy import shichi
+from .cepy import sici
+from .cepy import spence
+from .cepy import stdtr
+from .cepy import stdtri
+from .cepy import onef2
+from .cepy import threef0
+from .cepy import struve
+from .cepy import yv
+from .cepy import yn
+from .cepy import zeta
+from .cepy import zetac
+
+
+
+__all__ = [
+    'bdtr',
+    'betaln',
+    'btdtr',
+    'gamln',
+    'gammainc',
+    'ndtri',
+    'pdtr',
+]
